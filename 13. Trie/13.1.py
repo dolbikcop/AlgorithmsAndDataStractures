0affb34c1ad28b5f6e1e1c88f1984ef1798ff6b5
Polina <polyabikka@gmail.com>
2023-06-05 15:56:00 +0500

    DP3

--- 13. Trie/13.1.py
+++ 13. Trie/13.1.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def main():
     not_exit = True
     while not_exit:
@@ -9,6 +12,36 @@ def main():
         else:
             pass # get
 
+class Node:
+    def __init__(self):
+        self.alphabet = [0] * 256
+        self.is_end = False
+
+    def get_child(self, symbol: str):
+        index = ord(symbol)
+        if self.alphabet[index] == 0:
+            self.alphabet[index] = Node()
+        return self.alphabet[index]
+
 class Trie:
     def __init__(self):
-        self.
+        self.root = Node()
+
+    def add(self, word: str):
+        child = self.root
+        for i in word:
+            child = child.get_child(i)
+        child.is_end = True
+
+    def print_child(self, word: str, count: int):
+        c_node = self.root
+        for i in word:
+            c_node = c_node.get_child(i)
+
+        while count != 0:
+            for i in range(0, 256):
+                if c_node.alphabet[i] != 0:
+                        c_node.
+    def __print_child(self, results: List[str], word: List[str], count: int, node):
+        if node != 0:
+            
--- DP. Dynamic Programming/DP3.py
+++ DP. Dynamic Programming/DP3.py
@@ -1,2 +1,44 @@
 def main():
+    substrings = []
     string = input()
+    length = len(string)
+    k = 1
+    for i in range(0, length):
+        substrings.append([0] * (length - i))
+        for j in range(0, length - i):
+            sub = string[j:(j + i + 1)]
+            if i + 1 > 4:
+                sub, k = collapse(sub, i + 1)
+                sub = collapse_by_substrings(sub, substrings, len(sub), j)
+            substrings[i][j] = sub if k == 1 else f'{k}({sub})'
+    print(*substrings[-1])
+
+
+# aaaaaxaxax
+# абабабавававабабабававав
+def collapse_by_substrings(string: str, substrings, length: int, index: int):
+    result = string
+    if length > 4:
+        for end in range(0, length-1):
+            left = substrings[end][index]
+            right = substrings[length - end - 2][index + end + 1]
+            if len(left) + len(right) < len(result):
+                result = left + right
+    return result
+
+
+def collapse(string: str, length: int):
+    res_sub = string
+    k = 1
+    for i in range(1, length // 2 + 1):
+        if length % i != 0:
+            continue
+        sub = string[:i]
+        if string == sub * (length // i) and i + 3 < length:
+            res_sub = sub
+            k = length // i
+            break
+    return res_sub, k
+
+
+main()
